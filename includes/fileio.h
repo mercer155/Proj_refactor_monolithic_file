5c331450dba1a44393bf03acc094fe72ff0dc540
mercer155 <paluzzi.mp@gmail.com>
2020-09-25 10:50:19 -0400

    Finished

---
+++ includes/fileio.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#pragma once
+
+
+#include "../includes/constants.h"
+
+//attempt to open file 'filename' and read in all data
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int loadData(const std::string filename, std::vector<process> &myProcesses);
+
+//attempt to create or open file 'filename' to write all data to
+//returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
+int saveData(const std::string filename, std::vector<process> &myProcesses);
